47e2ab658e7629dae34730344e8723670e8d93d8
Yeshwanth D S <yeshhwanth93@gmail.com>
2026-05-22 19:36:00 -0700

    psleep 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/psleep.rb
+++ Formula/psleep.rb
@@ -1,8 +1,8 @@
 class Psleep < Formula
   desc "A drop-in replacement for sleep with a configurable progress bar"
   homepage "https://github.com/Yesh-02/psleep"
-  url "https://github.com/Yesh-02/psleep/releases/download/v0.2.4/psleep-v0.2.4-x86_64-apple-darwin.tar.gz"
-  sha256 "f6de533af6fd9965aed09c592fc2d99ee89eb326743eac4f0f104632a1117026"
+  url "https://github.com/Yesh-02/psleep/releases/download/v0.3.0/psleep-v0.3.0-x86_64-apple-darwin.tar.gz"
+  sha256 "4c084f0f05ba0e26948a7f21389588eded15f7c6c1769fb9061be76c07bc91e0"
   license "Apache-2.0"
 
   def install
